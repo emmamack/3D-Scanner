56f3f6e87498f38c094b31f30589679aa1730d92
Emma Mack <emack@olin.edu>
2019-09-13 17:19:31 -0400

    Neatened code

--- collect_data/collect_data.ino
+++ collect_data/collect_data.ino
@@ -1,36 +1,37 @@
-//servo package
+//sudo chmod a+rw /dev/ttyACM0
 
+//servo package
 #include <Servo.h>
-Servo h_Servo;
-Servo v_Servo;
+Servo h_servo;
+Servo v_servo;
 int h_servo_pin = 9;
 int v_servo_pin = 10;
+const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
+
 int h_angle = 0;
 int v_angle = 0;
 int v_current_angle = 0;
 int spd = 15;
 int dir = 1;
+
 int width = 90;
 int height = 10;
+
 int total_height = 100;
 bool keep_going = true;
-const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
-int sensorValue = 0;        // value read from the pot
+
+int sensorValue = 0;        // value read
 
 
-void setup() {
-  
-  //pinMode(motor, OUTPUT);  
-  h_Servo.attach(h_servo_pin);
-  v_Servo.attach(v_servo_pin);
+void setup() { 
+  h_servo.attach(h_servo_pin);
+  v_servo.attach(v_servo_pin);
   Serial.begin(9600);
 }
 
 void goToOrigin(){ //send sensor to origin
-  h_angle = 0;
-  v_angle = 360;
-  h_Servo.write(h_angle);
-  v_Servo.write(v_angle);
+  h_servo.write(0);
+  v_servo.write(0);
 }
 
 void loop() {
@@ -38,7 +39,7 @@ void loop() {
  //move horizontal motor one direction
  for (h_angle = 0; h_angle <= width; h_angle += dir) { // goes from 0 degrees to 180 degrees  
     // in steps of 1 degree
-    h_Servo.write(h_angle);              // tell servo to go to position in variable 'pos'
+    h_servo.write(h_angle);              // tell servo to go to position in variable 'pos'
     delay(spd);                       // waits 15ms for the servo to reach the position
     
     // read the analog in value:
@@ -54,7 +55,7 @@ void loop() {
  //move vertical motor in one direction
  for (v_angle = v_current_angle; v_angle <= v_current_angle + height -1; v_angle += 1) { // goes from 0 degrees to 180 degrees  
     // in steps of 1 degree
-    v_Servo.write(v_angle);              // tell servo to go to position in variable 'pos'
+    v_servo.write(v_angle);              // tell servo to go to position in variable 'pos'
     Serial.print("v_angle = ");
     Serial.println(v_angle);
     delay(spd);                       // waits 15ms for the servo to reach the position
@@ -66,7 +67,7 @@ void loop() {
  //move horizontal motor back
  for (h_angle = width; h_angle >= 0; h_angle += dir) { // goes from 0 degrees to 180 degrees  
     // in steps of 1 degree
-    h_Servo.write(h_angle);              // tell servo to go to position in variable 'pos'
+    h_servo.write(h_angle);              // tell servo to go to position in variable 'pos'
     delay(spd);                       // waits 15ms for the servo to reach the position
     
     // read the analog in value:
@@ -85,7 +86,7 @@ void loop() {
  //move vertical motor in same direction
  for (v_angle = v_current_angle; v_angle <= v_current_angle + height -1; v_angle += 1) { // goes from 0 degrees to 180 degrees  
     // in steps of 1 degree
-    v_Servo.write(v_angle);              // tell servo to go to position in variable 'pos'
+    v_servo.write(v_angle);              // tell servo to go to position in variable 'pos'
     Serial.print("v_angle = ");
     Serial.println(v_angle);
     delay(spd);                       // waits 15ms for the servo to reach the position
@@ -95,7 +96,7 @@ void loop() {
 
  if (v_current_angle >= total_height) {
   Serial.println("In exit statement!");
-  keep_going = false;
+  goToOrigin();
   break;
  }
  }
