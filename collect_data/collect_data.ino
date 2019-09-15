78b2a54cdf0ced03b8ed1aa209635670b89cf73c
Emma Mack <emack@olin.edu>
2019-09-15 12:36:38 -0400

    Move main loop actions into functions (UNTESTED)

--- collect_data/collect_data.ino
+++ collect_data/collect_data.ino
@@ -1,7 +1,6 @@
 //sudo chmod a+rw /dev/ttyACM0
 
-//servo package
-#include <Servo.h>
+#include <Servo.h>    //servo package
 Servo h_servo;
 Servo v_servo;
 int h_servo_pin = 9;
@@ -11,16 +10,15 @@ const int analogInPin = A0;  // Analog input pin that the potentiometer is attac
 int h_angle = 0;
 int v_angle = 0;
 int v_current_angle = 0;
-int spd = 15;
-int dir = 1;
 
+int spd = 15;
 int width = 90;
 int height = 10;
-
 int total_height = 100;
+
 bool keep_going = true;
 
-int sensorValue = 0;        // value read
+int sensorValue = 0; 
 
 
 void setup() { 
@@ -29,15 +27,13 @@ void setup() {
   Serial.begin(9600);
 }
 
-void goToOrigin(){ //send sensor to origin
+void goToOrigin(){    //send sensor to origin
   h_servo.write(0);
   v_servo.write(0);
 }
 
-void loop() {
- while (keep_going = true) {
- //move horizontal motor one direction
- for (h_angle = 0; h_angle <= width; h_angle += dir) { // goes from 0 degrees to 180 degrees  
+void move_h_cw() {    //move horizontal motor in clockwise direction
+  for (h_angle = 0; h_angle <= width; h_angle += 1) { // goes from 0 degrees to 180 degrees  
     // in steps of 1 degree
     h_servo.write(h_angle);              // tell servo to go to position in variable 'pos'
     delay(spd);                       // waits 15ms for the servo to reach the position
@@ -48,24 +44,11 @@ void loop() {
     Serial.print(sensorValue);
     Serial.println(",");
     //might need extra delay for python sending?
-
  }
- dir = dir * (-1);
+}
 
- //move vertical motor in one direction
- for (v_angle = v_current_angle; v_angle <= v_current_angle + height -1; v_angle += 1) { // goes from 0 degrees to 180 degrees  
-    // in steps of 1 degree
-    v_servo.write(v_angle);              // tell servo to go to position in variable 'pos'
-    Serial.print("v_angle = ");
-    Serial.println(v_angle);
-    delay(spd);                       // waits 15ms for the servo to reach the position
- }
- v_current_angle = v_angle;
- Serial.println(v_current_angle);
-
-
- //move horizontal motor back
- for (h_angle = width; h_angle >= 0; h_angle += dir) { // goes from 0 degrees to 180 degrees  
+void move_h_ccw() {
+  for (h_angle = width; h_angle >= 0; h_angle += -1) { // goes from 0 degrees to 180 degrees  
     // in steps of 1 degree
     h_servo.write(h_angle);              // tell servo to go to position in variable 'pos'
     delay(spd);                       // waits 15ms for the servo to reach the position
@@ -81,10 +64,10 @@ void loop() {
     Serial.println(",");
     //might need extra delay for python sending?
  }
- dir = dir * (-1);
+}
 
- //move vertical motor in same direction
- for (v_angle = v_current_angle; v_angle <= v_current_angle + height -1; v_angle += 1) { // goes from 0 degrees to 180 degrees  
+void move_v {   //move vertical motor
+  for (v_angle = v_current_angle; v_angle <= v_current_angle + height -1; v_angle += 1) { // goes from 0 degrees to 180 degrees  
     // in steps of 1 degree
     v_servo.write(v_angle);              // tell servo to go to position in variable 'pos'
     Serial.print("v_angle = ");
@@ -93,6 +76,18 @@ void loop() {
  }
  v_current_angle = v_angle;
  Serial.println(v_current_angle);
+}
+
+void checkEnd {     //check if 
+
+void loop() {
+ while (keep_going = true) {
+   move_h_cw()
+   move_v()
+   move_h_ccw()
+   move_v()
+ 
+ 
 
  if (v_current_angle >= total_height) {
   Serial.println("In exit statement!");
@@ -104,5 +99,3 @@ void loop() {
  //do nothing forever
  while(1); {}
 }
-
-//h_angle + width*dir
