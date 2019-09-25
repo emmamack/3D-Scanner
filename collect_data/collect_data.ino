f4aae9fe6a691de02500d9e34db20320dbe0eedb
Emma Mack <emack@olin.edu>
2019-09-25 10:55:05 -0400

    Add resolution parameter

--- collect_data/collect_data.ino
+++ collect_data/collect_data.ino
@@ -1,5 +1,11 @@
 //sudo chmod a+rw /dev/ttyACM0
 
+//parameters describing sensor position and target size
+float d = 1;
+float w = 0.3;
+float h = 0.2;
+float scan_resolution = 1; //in integers: best is 1, higher numbers --> lower res
+
 #include <Servo.h>    //servo package
 Servo h_servo;
 Servo v_servo;
@@ -15,12 +21,6 @@ int delay_time = 15;
 int width = 90;
 int height = 2;
 int total_height = 60;
-
-//parameters describing sensor position and target size
-float d = 1;
-float w = 0.3;
-float h = 0.2;
-float seg_inc = 0.05;
 
 bool keep_going = true;
 
@@ -64,7 +64,7 @@ void sendData() {
 
 //move horizontal motor in clockwise direction
 void moveHCW() {
-  for (h_angle = 0; h_angle <= width; h_angle += 2) { // goes from 0 degrees to 180 degrees  
+  for (h_angle = 0; h_angle <= width; h_angle += scan_resolution) { // goes from 0 degrees to 180 degrees  
     // in steps of 1 degree
     h_servo.write(h_angle);              // tell servo to go to position in variable 'pos'
     delay(delay_time);                       // waits 15ms for the servo to reach the position
@@ -75,7 +75,7 @@ void moveHCW() {
 
 //move horizontal motor in counter-clockwise direction
 void moveHCCW() {
-  for (h_angle = width; h_angle >= 0; h_angle += -2) { // goes from 0 degrees to 180 degrees  
+  for (h_angle = width; h_angle >= 0; h_angle += -1*scan_resolution) { // goes from 0 degrees to 180 degrees  
     // in steps of 1 degree
     h_servo.write(h_angle);              // tell servo to go to position in variable 'pos'
     delay(delay_time);                       // waits delay time for the servo to reach the position
@@ -86,7 +86,7 @@ void moveHCCW() {
 
 //move vertical motor
 void moveV() {
-  for (v_angle = v_current_angle; v_angle <= v_current_angle + height -1; v_angle += 1) { // goes from current angle up height interval  
+  for (v_angle = v_current_angle; v_angle <= v_current_angle + scan_resolution -1; v_angle += 1) { // goes from current angle up height interval  
     // in steps of 1 degree
     v_servo.write(v_angle);              // tell servo to go to position in variable 'pos'
     delay(delay_time);                       // waits 15ms for the servo to reach the position
